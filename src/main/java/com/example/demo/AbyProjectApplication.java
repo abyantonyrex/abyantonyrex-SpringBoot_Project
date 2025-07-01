c135bdb6f073f551b1d33942b1d6055b01e786ac
abyantonyrex <abyrex2002@gmail.com>
2025-07-01 20:27:42 +0530

    Fix: bugs

--- src/main/java/com/example/demo/AbyProjectApplication.java
+++ src/main/java/com/example/demo/AbyProjectApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class AbyProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AbyProjectApplication.class, args);
+		Dotenv dotenv = Dotenv.load();
+	    System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+	    System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+	    System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+	    SpringApplication.run(AbyProjectApplication.class, args);
 	}
 
 }
